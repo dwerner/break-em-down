16cb748302f389d502940678cb4ecdf81525b627
Ian Lovett <i.c.lovett@gmail.com>
2013-10-09 21:01:41 -0700

    Added GameBall.  Moved Quad back so GameBall is not clipping

---
+++ ballbreaker/Assets/Scripts/GameBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBall : MonoBehaviour {
+	
+	public float minSpeed = 15;
+	public float maxSpeed = 30;
+	public float curSpeed = 15;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddRelativeForce(new Vector3(0, this.minSpeed, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		Debug.Log("Collided with " + col.gameObject.name);
+	}
+}
