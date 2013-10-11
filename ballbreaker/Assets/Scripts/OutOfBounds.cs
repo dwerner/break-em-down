3e0957712f8da38a7596f9969eb09ebe94ad0abf
daniel.werner@dena.jp <daniel.werner@dena.jp>
2013-10-11 16:20:21 -0700

    Fixing formatting for project.

--- ballbreaker/Assets/Scripts/OutOfBounds.cs
+++ ballbreaker/Assets/Scripts/OutOfBounds.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class OutOfBounds : MonoBehaviour {
-
-	void OnCollisionEnter(Collision c){
+	void OnCollisionEnter (Collision c) {
 		Application.LoadLevel ("MainMenu");
 	}
 }
