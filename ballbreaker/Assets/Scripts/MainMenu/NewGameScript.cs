361c99817ccd474a85dfe1779ae8a2404eb5d771
dan.werner@gmail.com <dan.werner@gmail.com>
2013-10-13 14:08:39 -0700

    Adding scenes to build settings

--- ballbreaker/Assets/Scripts/MainMenu/NewGameScript.cs
+++ ballbreaker/Assets/Scripts/MainMenu/NewGameScript.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class NewGameScript : MonoBehaviour {
 	
+   public string levelName;
+
 	void OnMouseUpAsButton(){
-		Application.LoadLevel("tribal");
+      if (levelName.Trim() != string.Empty) {
+         Application.LoadLevel(this.levelName);
+
+      }
 	}
 }
