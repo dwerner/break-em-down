cc1338078bc9338bef7bfa6b09eb72deffe44dbc
daniel.werner@dena.jp <daniel.werner@dena.jp>
2013-10-12 23:10:27 -0700

    Fixing formatting

--- ballbreaker/Assets/Scripts/PlayerPaddle.cs
+++ ballbreaker/Assets/Scripts/PlayerPaddle.cs
@@ -4,23 +4,23 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class PlayerPaddle : MonoBehaviour {
-	public float speed = 20;
-	public float leftLimit = -7.5f;
-	public float rightLimit = 7.5f;
-	public bool touchesExist;
-	
+   public float speed = 20;
+
+   public float leftLimit = -7.5f;
+
+   public float rightLimit = 7.5f;
+
+   public bool touchesExist;
 
    //Changing this into an IEnumerator - so re-entry happens once per frame, and Update is never needed
-	IEnumerator Start () {
-
-
+   IEnumerator Start() {
       yield return StartCoroutine(this.GameLoop());
-	}
+   }
 
-   IEnumerator GameLoop(){
+   IEnumerator GameLoop() {
       Vector3 pos = rigidbody.position;
       while (true) {
-         var h = Input.GetAxis ("Horizontal");
+         var h = Input.GetAxis("Horizontal");
 
          if (h != 0) {
             var move = h * Time.deltaTime * speed;
@@ -33,11 +33,11 @@ public class PlayerPaddle : MonoBehaviour {
              * 
              * *Note that changing the masses of the targets did have an effect on the kinds of intersections that were allowed before the object rebounds.
              */
-            if (Mathf.Abs (h) > 0) {
+            if (Mathf.Abs(h) > 0) {
                if (!(this.rigidbody.position.x + move < this.leftLimit) &&
                    !(this.rigidbody.position.x + move > this.rightLimit)) { 
 
-                  this.rigidbody.position += new Vector3 (move, 0.0f, 0.0f);
+                  this.rigidbody.position += new Vector3(move, 0.0f, 0.0f);
                }
             }
          }
@@ -51,7 +51,7 @@ public class PlayerPaddle : MonoBehaviour {
           * So the paddle moves with an offset of 1/2 the x+y resolution.
           * 
           */
-         if (Input.touches.Count () > 0) {
+         if (Input.touches.Count() > 0) {
             var delta = Input.touches[0].position;
 
             var movingTouches = from x in Input.touches
@@ -60,7 +60,7 @@ public class PlayerPaddle : MonoBehaviour {
 
 
 
-            rigidbody.position = new Vector3 ((delta.x - 400f) / 80f, pos.y, pos.z);
+            rigidbody.position = new Vector3((delta.x - 400f) / 80f, pos.y, pos.z);
          }
 
          yield return null;
