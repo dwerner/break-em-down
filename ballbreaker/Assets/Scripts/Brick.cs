4ebdf28a771995eda2a8f5391f011839f6fb09a6
dan.werner@gmail.com <dan.werner@gmail.com>
2013-10-13 17:43:48 -0700

    starting GameController

--- ballbreaker/Assets/Scripts/Brick.cs
+++ ballbreaker/Assets/Scripts/Brick.cs
@@ -10,21 +10,28 @@ public class Brick : MonoBehaviour {
 
 		hitPoints -= 1;
 
-		var sizes = new List<float>() {
-			1.0f,1.1f,1.2f,1.3f,1.2f,1.1f,1.0f
-		};
+      yield return StartCoroutine(this.Pulse());
 
-		var initialScale = transform.localScale;
-
-		foreach (var i in sizes){
-			transform.localScale = initialScale * i;
-			yield return 0;
-		}
-	
 		if (hitPoints <= 0) {
-			this.gameObject.active = false;
+			this.gameObject.SetActive(false);
 
 		}
-		yield return 0;
+
+		yield return null;
 	}
+
+   IEnumerator Pulse(){
+      var sizes = new List<float>() {
+         1.0f,1.1f,1.2f,1.3f,1.2f,1.1f,1.0f
+      };
+
+      var initialScale = transform.localScale;
+
+      foreach (var i in sizes){
+         transform.localScale = initialScale * i;
+         yield return null;
+      }
+      yield return null;
+   }
+
 }
--- ballbreaker/Assets/Scripts/ExtensionMethods.cs
+++ ballbreaker/Assets/Scripts/ExtensionMethods.cs
@@ -1,10 +1,31 @@
 using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace AssemblyCSharp {
+public static class ExtensionMethods {
+   /*
+   public static IEnumerator Pulse(this MonoBehaviour mb) {
 
-   public class ExtensionMethods {
-      public ExtensionMethods() {
-      }
+         var sizes = new List<float>() {
+            1.0f,1.1f,1.2f,1.3f,1.2f,1.1f,1.0f
+         };
+
+         yield return mb.StartCoroutine(mb.Pulse(sizes));
+         
    }
+
+   public static IEnumerator Pulse(this MonoBehaviour mb, IEnumerable<float> sizes) {
+
+      var initialScale = mb.transform.localScale;
+
+      foreach (var i in sizes){
+         mb.transform.localScale = initialScale * i;
+         yield return null;
+      }
+
+      yield return null;
+   }
+   */
 }
 
