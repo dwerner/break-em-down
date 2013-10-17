99291bc13394ea02fc3738351b9eaa862cd1e616
Ian Lovett <ian@ilovettstudios.com>
2013-10-16 21:41:32 -0700

    Added a touch test scene... pillbury doughboy wont show up properly... what gives?

---
+++ ballbreaker/Assets/Scripts/ObjectBehaviours/MouseButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseButton : MonoBehaviour {
+
+   void OnMouseDown() {
+      Debug.Log("Mousedown on: " + this.name);
+   }
+
+   void OnMouseUp() {
+      Debug.Log("Mouseup on: " + this.name);
+   }
+
+}
