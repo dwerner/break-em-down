09ca7932512f82b6429feaf0658c875f0d4564ce
dan.werner@gmail.com <dan.werner@gmail.com>
2013-11-21 20:49:54 -0800

    some changes...

--- ballbreaker/Assets/Scripts/LevelController.cs
+++ ballbreaker/Assets/Scripts/LevelController.cs
@@ -23,6 +23,16 @@ public class LevelController : MonoBehaviour {
       new List<string>(){
          "level1", 
          "level2",
+      "level3",
+      "level4",
+      "level5",
+      "level6",
+      "level7",
+      "level8",
+      "level9",
+      "level10",
+      "level11",
+      "level12",
       };
 
    private string currentLevel;
