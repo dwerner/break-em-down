2f330eaf23281c1ef42e1cfbf2dc31c2eb40601b
Daniel Werner <dan@pewpew.in>
2013-11-30 15:33:16 -0800

    Adding ingame menu - nonfunctional for demo

--- ballbreaker/Assets/Scripts/LevelController.cs
+++ ballbreaker/Assets/Scripts/LevelController.cs
@@ -5,105 +5,127 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class LevelController : MonoBehaviour {
-   public GameObject ballPrefab;
+  public GameObject ballPrefab;
+  public GameObject inGameMenu;
 
-   public event BallOutOfBoundsEventHandler BallOutOfBounds;
-   public delegate void BallOutOfBoundsEventHandler(object sender);
+  public event BallOutOfBoundsEventHandler BallOutOfBounds;
+  public delegate void BallOutOfBoundsEventHandler(object sender);
 
-   public event BrickDestroyedEventHandler BrickDestroyed;
-   public delegate void BrickDestroyedEventHandler(object sender);
+  public event BrickDestroyedEventHandler BrickDestroyed;
+  public delegate void BrickDestroyedEventHandler(object sender);
 
-   public event EventHandler GameOver;
+  public event EventHandler GameOver;
+  public event EventHandler LevelWon;
 
-   public event EventHandler LevelWon;
+  public int brickCount;
 
-   public int brickCount;
+  private static LevelController instance;
+  public static LevelController getInstance(){
+    return instance;
+  }
 
-   private static LevelController instance;
-   public static LevelController getInstance(){
-      return instance;
-   }
+  private List<string> levels =
+    new List<string>(){
+      "level1", 
+      "level2",
+      "level3",
+      "level4",
+      "level5",
+      "level6",
+      "level7"
+    };
 
-   private List<string> levels =
-      new List<string>(){
-         "level1", 
-         "level2",
-	      "level3",
-	      "level4",
-	      "level5",
-	      "level6",
-	      "level7"
-      };
+  private string currentLevel;
 
-   private string currentLevel;
+  IEnumerator Start() {
+    LevelController.instance = this;
+    this.currentLevel = Application.loadedLevelName;
+    Debug.Log(this.currentLevel);
 
-   void Start() {
-      LevelController.instance = this;
-      this.currentLevel = Application.loadedLevelName;
-      Debug.Log(this.currentLevel);
-   }
-
-   public void RaiseGameOver() {
-
-      Debug.Log("-- game over --");
-      Application.LoadLevel("MainMenu");
-
-      EventHandler handler = GameOver;
-      if (handler != null) {
-         handler(this, new System.EventArgs());
+    while (true) {
+      if (Input.GetKeyDown(KeyCode.Escape)){
+        if (this.isPaused){
+          this.inGameMenu.SetActive(false);
+          this.Resume();
+          Debug.Log("unpaused");
+        } else {
+          this.Pause();
+          this.inGameMenu.SetActive(true);
+          Debug.Log("pause");
+        }
       }
+      yield return null;
+    }
+  }
 
-   }
+  public void RaiseGameOver() {
 
-   private string getNextLevel(){
-      int idx = levels.IndexOf(this.currentLevel);
-      if (idx+1 == levels.Count) {
-         return "MainMenu";
-      }
-      else {
-         return levels[idx + 1];
-      }
-   }
+    Debug.Log("-- game over --");
+    Application.LoadLevel("MainMenu");
 
-   public void RaiseLevelWon() {
+    EventHandler handler = GameOver;
+    if (handler != null) {
+      handler(this, new System.EventArgs());
+    }
 
-      Debug.Log("-- level won --");
-      Application.LoadLevel(this.getNextLevel());
+  }
 
-      EventHandler handler = LevelWon;
-      if (handler != null) {
-         handler(this, new System.EventArgs());
-      }
+  private string getNextLevel(){
+    int idx = levels.IndexOf(this.currentLevel);
+    if (idx+1 == levels.Count) {
+      return "MainMenu";
+    }
+    else {
+      return levels[idx + 1];
+    }
+  }
 
-   }
+  public void RaiseLevelWon() {
+
+    Debug.Log("-- level won --");
+    Application.LoadLevel(this.getNextLevel());
+
+    EventHandler handler = LevelWon;
+    if (handler != null) {
+      handler(this, new System.EventArgs());
+    }
+
+  }
+
+  public void Pause() {
+    Time.timeScale = 0;
+  }
+
+  public bool isPaused { get { return Time.timeScale == 0; } }
+
+  public void Resume () {
+    Time.timeScale = 1;
+  }
 
 
-   public void RaiseBallOutOfBounds() {
-      BallOutOfBoundsEventHandler handler = BallOutOfBounds;
+  public void RaiseBallOutOfBounds() {
+    BallOutOfBoundsEventHandler handler = BallOutOfBounds;
+    Debug.Log("ball lost");
 
-      Debug.Log("ball lost");
+    if (handler != null) {
+      handler(this);
+    }
 
-      if (handler != null) {
-         handler(this);
+    Instantiate(this.ballPrefab);
+  }
 
-      }
+  public void RaiseBrickDestroyed() {
 
-      Instantiate(this.ballPrefab);
-   }
+    this.brickCount -= 1;
 
-   public void RaiseBrickDestroyed() {
+    BrickDestroyedEventHandler handler = BrickDestroyed;
+    if (handler != null) {
+      handler(this);
+    }
 
-      this.brickCount -= 1;
-
-      BrickDestroyedEventHandler handler = BrickDestroyed;
-      if (handler != null) {
-         handler(this);
-
-      }
-
-      if (this.brickCount == 0) {
-         Debug.Log("Out of bricks...");
-         this.RaiseLevelWon();
-      }
-   }
+    if (this.brickCount == 0) {
+      Debug.Log("Out of bricks...");
+      this.RaiseLevelWon();
+    }
+  }
 }
